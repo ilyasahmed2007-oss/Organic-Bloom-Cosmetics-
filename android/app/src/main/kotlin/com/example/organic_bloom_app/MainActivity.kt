9aa3a8ab9c34fa740dba5262b509304483f83c4f
ilyasahmed2007-oss <ilyasahmed2007@gmail.com>
2026-06-04 00:50:57 +0500

    Update and rename mainactivity.kt to MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/organic_bloom_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.organic_bloom_cosmetics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/organic_bloom_app/mainactivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.organic_bloom_cosmetics
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
